ea03909c53b530728561f136c03873ce9b6b530a
Mike Holloway <me@mikeholloway.co.uk>
2020-04-08 11:31:52 +0100

    Updated deps, tweaks to how the CMS previews get loaded

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,6 @@
 
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import BlogPreview from './preview-templates/BlogPreview'
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('blog', BlogPreview)
